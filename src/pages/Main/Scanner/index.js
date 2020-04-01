2555b0d11070cec8f5a985afd3f0cd7b1b65ecf9
Wanderson <wandersonce1@hotmail.com>
2020-03-31 23:53:40 -0700

    Apling Quagga Api to read ISBN Code

---
+++ src/pages/Main/Scanner/index.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react';
+import Quagga from 'quagga';
+import { validateIsbn } from '../../../services/books'
+import { Container, Video, ScanMarker } from './styles';
+
+function Scanner() {
+    let scannerAtteps = 0;
+
+    const onDetected = result => {
+        Quagga.offDetected(onDetected); // * It will stop our function when find a code
+
+        const isbn = result.codeResult.code;
+        console.log(isbn);
+
+        if (validateIsbn(isbn)) {
+            alert(isbn);
+            return;
+        } else {
+            if (scannerAtteps >= 5) {
+                alert("Wasn't possible to read your code, please try again!")
+            }
+        }
+
+        scannerAtteps++;
+        Quagga.onDetected(onDetected);
+    }
+
+    useEffect(() => {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {  // *check if your browser can open the camera
+            Quagga.init(
+                {
+                    inputStream: {
+                        name: "Live",
+                        type: "LiveStream",
+                        target: document.querySelector("#video"),
+                        constraints: {
+                            facingMode: "environment",
+                        },
+                    },
+                    numOfWorkers: 1,
+                    locate: true,
+                    decoder: {
+                        readers: ["ean_reader"],
+                    }
+                },
+                err => {
+                    if (err) {
+                        console.error(err);
+                        alert("Error to open the camera, give the right permision");
+                        return;
+                    };
+                    Quagga.start();
+                }
+            );
+            Quagga.onDetected(onDetected);
+        }
+    }, []);
+
+    return (
+        <>
+            <Video id="video" />
+            <Container>
+                <ScanMarker>
+                    <img src="../../../../assets/scan-marker.png"
+                        alt="Reader code"
+                        width="260"
+                        height="260"
+                    />
+                    <p className="label">Show your book code</p>
+                </ScanMarker>
+                <img className="logo"
+                    src="../../../../assets/signature.png"
+                    alt="Wanderson Castro"
+                    width="137"
+                    height="69"
+                />
+            </Container>
+        </>
+    );
+}
+
+export default Scanner;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
-
-import { Container } from './styles';
+import Scanner from './Scanner/index';
 
 function Main() {
-  return (
-    <Container>
-      <img
-        src="https://devsamurai-materials.s3.amazonaws.com/templates/dev-samurai-white.png"
-        height="256"
-        alt="Dev Samurai"
-      />
-      <h1>Fala Samurai!</h1>
-      <p>Esse é o template básico da Dev Samurai para React.</p>
-    </Container>
-  );
+  return <Scanner />
 }
 
 export default Main;
